34d8676166052828cf161d197ade201f0e926f22
Jimmy Zhang <jimmyzhang1@hotmail.com>
2025-08-20 12:21:57 -0700

    Add constants + types files

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,5 +1,6 @@
 import Navbar from "~/components/Navbar";
 import type { Route } from "./+types/home";
+import { resumes } from "~/constants";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -17,5 +18,11 @@ export default function Home() {
         <h2>Review your submissions and check AI-powered feedback.</h2>
       </div>
     </section>
+    {resumes.map((resume) => (
+      <div>
+        <h1>{resume.jobTitle}</h1>
+      </div>
+    ))}
   </main>;
+
 }
