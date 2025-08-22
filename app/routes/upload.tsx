ca46c55fc7af4ac8ef9e74b248b8d96e62d750f3
Jimmy Zhang <jimmyzhang1@hotmail.com>
2025-08-22 15:55:50 -0700

    Fix bug + add navigate to uuid

--- app/routes/upload.tsx
+++ app/routes/upload.tsx
@@ -73,10 +73,12 @@ const upload = () => {
         : feedback.message.content[0].text;
 
     data.feedback = JSON.parse(feedbackText)
-    await kv.set(`resume: ${uuid}`, JSON.stringify(data))
+    await kv.set(`resume:${uuid}`, JSON.stringify(data))
     setStatusText('Analysis complete, redirecting...')
 
     console.log(data)
+    navigate(`/resume/${uuid}`)
+
   };
 
   const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
