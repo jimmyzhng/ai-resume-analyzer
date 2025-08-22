7f724386e16f475273e7ffad396dc1cfbc2364b5
Jimmy Zhang <jimmyzhang1@hotmail.com>
2025-08-22 14:02:37 -0700

    Create utils file

--- app/components/FileUploader.tsx
+++ app/components/FileUploader.tsx
@@ -1,16 +1,7 @@
 import React, { useState } from "react";
 import { useCallback } from "react";
 import { useDropzone } from "react-dropzone";
-
-const formatSize = (bytes: number): string => {
-  if (bytes === 0) return "0 Bytes";
-
-  const k = 1024;
-  const sizes = ["Bytes", "KB", "MB", "GB"];
-  const i = Math.floor(Math.log(bytes) / Math.log(k));
-
-  return parseFloat((bytes / Math.pow(k, i)).toFixed(2)) + " " + sizes[i];
-};
+import { formatSize } from "~/lib/utils";
 
 interface FileUploaderProps {
   onFileSelect?: (file: File | null) => void;
